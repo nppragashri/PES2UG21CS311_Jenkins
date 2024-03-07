6413158a65677a02f6abd299a26b4cd7dfa67320
nppragashri <nppragashri@gmail.com>
2024-03-07 10:58:59 +0530

    added changes to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World!\n";
   cout << "Hello, Jenkins\n";
+  count << "Hello Pragashri\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
